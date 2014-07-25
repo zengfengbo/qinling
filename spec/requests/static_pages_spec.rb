f4e7e542158342cc75576e124576cf387a2f7dcf
bosh <boshgc@gmail.com>
2014-07-25 16:29:51 +0800

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,26 +1,22 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  subject { page }
   describe "Home page" do
-    it "should have the content 'Qinling'" do
-      visit "/static_pages/home"
-      expect(page).to have_content("Qinling")
-    end
-    it "should have the title 'Qinling'" do
-      visit "/static_pages/home"
-      expect(page).to have_title("Qinling")
-    end
+    before { visit root_path }
+    it { should have_content("Sample App")}
+    it { should have_title(full_title("")) }
+    it { should_not have_content("| Home")}
   end
+
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit "/static_pages/help"
-      expect(page).to have_content("bosh")
-    end
+    before { visit help_path }
+    it { should have_content("Help")}
+    it { should have_title(full_title("Help"))}
   end
   describe "About page" do
-    it "should have the content 'want'" do
-      visit "/static_pages/about"
-      expect(page).to have_content("Love")
-    end
+    before { visit about_path }
+    it { should have_content("About")}
+    it { should have_title(full_title("About us"))}
   end
 end
