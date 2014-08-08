e59bab07416ad6094d0800a2e720ccfecb40040a
bosh <boshgc@gmail.com>
2014-08-09 03:10:38 +0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Qinling::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destory]
   get "users/new"
   # match "/", to: 'static_pages#home', via: 'get'
   root to: 'static_pages#home'
+  match "/signin", to:'sessions#new', via: 'get'
+  match "/signout", to:'sessions#destory', via: 'delete'
   match "/signup", to:'users#new', via: 'get'
   match "/home", to: 'static_pages#home', via: 'get'
   match "/help", to: 'static_pages#help', via: 'get'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ describe User do
   it {should respond_to(:password_digest)}
   it {should respond_to(:password)}
   it {should respond_to(:password_confirmation)}
+  it {should respond_to(:remember_token)}
   it {should respond_to(:authenticate)}
   it {should be_valid}
   
@@ -89,7 +90,9 @@ foo@bar_baz.com foo@bar+baz.com]
       user_with_same_email.email = @user.email.upcase
     end
     it {should_not be_valid}
-
+  end
+  describe "remember token" do 
+    before {@user.save}
+    its(:remember_token) {should_not be_blank}
   end
 end
-
