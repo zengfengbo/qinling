e16cbafbf31c0fdb40780d97696514663b0d1b4b
bosh <boshgc@gmail.com>
2014-08-14 15:10:34 +0800

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,20 +5,37 @@ module SessionsHelper
     user.update_attribute(:remember_token, User.hash(remember_token))
     self.current_user = user
   end
+
   def signed_in?
     !current_user.nil?
   end
+
   def current_user=(user)
     @current_user = user
   end
+
   def current_user
     remember_token = User.hash(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
   end
+  
+  def current_user?(user)
+    user == current_user
+  end
+
   def sign_out
     current_user.update_attribute(:remember_token,
                                   User.hash(User.new_remember_token))
     self.current_user = nil
     cookies.delete(:remember_token)
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+  
+  def store_location 
+    session[:return_to] = request.fullpath if request.get?
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,7 @@ describe User do
                      password_confirmation: "foobar")
   end
   subject { @user }
-  
+
   it {should respond_to(:name)}
   it {should respond_to(:email)}
   it {should respond_to(:password_digest)}
@@ -16,29 +16,39 @@ describe User do
   it {should respond_to(:password_confirmation)}
   it {should respond_to(:remember_token)}
   it {should respond_to(:authenticate)}
+  it {should respond_to(:admin)}
   it {should be_valid}
-  
-  describe "with a password that's too short" do 
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+
+  describe "with a password that's too short" do
     before {@user.password = @user.password_confirmation = "a" * 5}
     it {should be_invalid}
   end
-  
+
   describe 'return value of authenticate method' do
     before {@user.save}
     let(:found_user) {User.find_by(email: @user.email)}
-    
+
     describe 'with valid password' do
       it {should eq found_user.authenticate(@user.password)}
     end
-    
+
     describe 'with invalid password' do
       let(:user_for_invalid_password) {found_user.authenticate("invalid")}
       it {should_not eq user_for_invalid_password}
       specify {expect(user_for_invalid_password).to be_false}
     end
   end
-  describe "when password is no present" do 
-    before do 
+  describe "when password is no present" do
+    before do
       @user = User.new(name: "Example User",
                        email: "user@example.com",
                        password: " ",
@@ -46,26 +56,26 @@ describe User do
     end
     it {should_not be_valid}
   end
-  
-  describe "when password doesn't match confirmation" do 
+
+  describe "when password doesn't match confirmation" do
     before {@user.password_confirmation = "mismatch"}
     it {should_not be_valid}
   end
-  
-  describe "when name is not present" do 
+
+  describe "when name is not present" do
     before {@user.name = " "}
     it {should_not be_valid}
   end
-  describe "when email is not present" do 
+  describe "when email is not present" do
     before {@user.email = " "}
     it {should_not be_valid}
   end
-  describe "when name is too long" do 
+  describe "when name is too long" do
     before {@user.name = "a"*51}
     it {should_not be_valid}
   end
   describe "when email format is invalid" do
-    it "should be invalid" do 
+    it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
 foo@bar_baz.com foo@bar+baz.com]
       addresses.each do |invalid_address|
@@ -74,8 +84,8 @@ foo@bar_baz.com foo@bar+baz.com]
       end
     end
   end
-  describe "when email format is valid" do 
-    it "should be valid" do 
+  describe "when email format is valid" do
+    it "should be valid" do
       addresses = %w[user@foo.COM A_US-er@f.b.org first.lst@foo.jp a+b@baz.cn]
       addresses.each do |valid_address|
         @user.email = valid_address
@@ -83,15 +93,15 @@ foo@bar_baz.com foo@bar+baz.com]
       end
     end
   end
-  describe "when email address is already taken" do 
-    before do 
+  describe "when email address is already taken" do
+    before do
       user_with_same_email = @user.dup
       user_with_same_email.save
       user_with_same_email.email = @user.email.upcase
     end
     it {should_not be_valid}
   end
-  describe "remember token" do 
+  describe "remember token" do
     before {@user.save}
     its(:remember_token) {should_not be_blank}
   end
